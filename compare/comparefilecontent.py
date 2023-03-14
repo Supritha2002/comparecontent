4cc7dd80524525ca8eb874d48f981c84ed72ee1d
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-14 13:04:11 +0530

    Update comparefilecontent.py

--- compare/comparefilecontent.py
+++ compare/comparefilecontent.py
@@ -1,13 +1,15 @@
 from dotenv import dotenv_values
+
+
 def readcontents(filename):
-    contents=dotenv_values(filename)
+    contents = dotenv_values(filename)
     return contents
 
 
-def comparefilecontent(filepath,standardfile):
-    filecontent=readcontents(filepath)
-    standardfilecontent=readcontents(standardfile)
-    missing_keys=[]
+def comparefilecontent(filepath, standardfile):
+    filecontent = readcontents(filepath)
+    standardfilecontent = readcontents(standardfile)
+    missing_keys = []
     for keys in standardfilecontent:
         if keys not in filecontent:
             missing_keys.append(keys)
