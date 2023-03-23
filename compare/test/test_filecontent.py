882419b30bcec8c00ba1f199316b1e3a9c4abf61
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-23 15:52:09 +0530

    Update test_filecontent.py

--- compare/test/test_filecontent.py
+++ compare/test/test_filecontent.py
@@ -1,4 +1,4 @@
-from comparefilecontent import comparefilecontent
+from compare_filecontent import comparefilecontent
 import unittest
 
 
@@ -6,8 +6,15 @@ class TestFolder(unittest.TestCase):
 
     def test_for_missing_filecontent(self):
         missingkeys = comparefilecontent(".env", "standard.env")
-        self.assertEqual(len(missingkeys) ,  0)
+        self.assertEqual(len(missingkeys) , 0)
+    
+    def test_invalid_folder_path(self):
+        comparefilecontent("env", "standard.env")
 
-
+    def test_invalid_standard_path(self):
+        comparefilecontent(".env", "standards.env")
+        
+ 
+     
 if __name__ == "__main__":
     unittest.main()
