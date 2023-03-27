f9c65148a7df1ae2c30b481a33b15a7669edb3ce
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-27 13:20:08 +0530

    Update test_filecontent.py

--- compare/test/test_filecontent.py
+++ compare/test/test_filecontent.py
@@ -9,10 +9,10 @@ class TestFolder(unittest.TestCase):
         self.assertEqual(len(missingkeys) , 0)
     
     def test_invalid_folder_path(self):
-        comparefilecontent("env", "standard.env")
+        self.assertRaises(SystemExit,comparefilecontent,"env", "standard.env")
 
     def test_invalid_standard_path(self):
-        comparefilecontent(".env", "standard.env")
+        self.assertRaises(SystemExit,comparefilecontent,".env", "standards.env")
         
  
      
