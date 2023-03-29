cc050f0d6a97b5231b806660a7537ccf3b9487e6
V S Supritha <103561315+Supritha2002@users.noreply.github.com>
2023-03-29 10:38:31 +0530

    Update test_filecontent.py

--- compare/test/test_filecontent.py
+++ compare/test/test_filecontent.py
@@ -4,9 +4,13 @@ import unittest
 
 class TestFolder(unittest.TestCase):
 
-    def test_for_missing_filecontent(self):
+   def test_for_no_missing_filecontent(self):
         missingkeys = comparefilecontent(".env", "standard.env")
-        self.assertEqual(len(missingkeys) , 0)
+        self.assertEqual(len(missingkeys), 0)
+ 
+    def test_for_missing_filecontent(self):
+        missingkeys = comparefilecontent(".env.common", "standard.env")
+        self.assertTrue(len(missingkeys) > 0)
     
     def test_invalid_folder_path(self):
         self.assertRaises(SystemExit,comparefilecontent,"env", "standard.env")
